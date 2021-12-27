0500610b2f8819805c1721337d34d594c9b2edb7
karin <krek250@gmail.com>
2021-12-27 12:53:24 +0200

    create mobile pages and test cases

---
+++ Test/test_mobile.py
@@ -0,0 +1,14 @@
+from Main.WorkFlows.mobile_wf import Mobile_WF
+
+
+class Test_Financial:
+
+    def test_1(self):
+        Mobile_WF.search("TVM Calculator")
+        Mobile_WF.click_on_waanted_result("'TVM Calculator'")
+        assert Mobile_WF.get_app_title("'TVM Calculator'") == "TVM Calculator"
+
+    def test_2(self):
+        Mobile_WF.navigate_to_home_page()
+        Mobile_WF.calculate("'6'", "'×'", "'5'")
+        assert Mobile_WF.get_result_text() == "30"
